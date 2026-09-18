2589e09d032b9231c0550b68ca94595d9f02ebcc
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-09-18 11:41:01 +0700

    menambahkan dependensi morgan untuk mempermudah pengembangan

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { auth } from "./routes/auth-routes.js";
 
 export const app = express();
 
+app.use(morgan("dev"));
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
