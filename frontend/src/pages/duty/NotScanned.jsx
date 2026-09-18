1d94165cd91552e3ef48c127d968e1f1292eaa31
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-09-18 20:27:56 +0700

    merapihkan kode di folder duty

--- frontend/src/pages/duty/NotScanned.jsx
+++ frontend/src/pages/duty/NotScanned.jsx
@@ -1,2 +1,4 @@
-import DutyDashboard from './DutyDashboard';
-export default function NotScanned(){return <DutyDashboard onlyNotScanned title="Siswa Belum Scan"/>;}
+import DutyDashboard from "./DutyDashboard";
+export default function NotScanned() {
+  return <DutyDashboard onlyNotScanned title="Siswa Belum Scan" />;
+}
