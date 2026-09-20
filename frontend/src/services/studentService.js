d66e5a00dcd081eb80dd154a9d83bee61a27c8c0
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-09-20 21:26:03 +0700

    fix bug pada studentService.js

--- frontend/src/services/studentService.js
+++ frontend/src/services/studentService.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 const studentService = {
   async getStudents() {
-    const response = await api.get("/students");
+    const response = await api.get("/admin/student");
     return response.data;
   },
 
