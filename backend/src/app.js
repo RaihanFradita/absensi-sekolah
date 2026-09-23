0c120282b760b3a2095764349aee714efccae37d
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-09-23 12:08:54 +0700

    membuat api untuk mengambil data kelas di route guru

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ import { auth } from "./routes/auth-routes.js";
 import { admin } from "./routes/admin-routes.js";
 import { student } from "./routes/student-routes.js";
 import { attandance } from "./routes/attendance-routes.js";
+import { teacher } from "./routes/teacher-routes.js";
 
 export const app = express();
 
@@ -28,4 +29,5 @@ app.use(express.json());
 app.use("/api/auth", auth);
 app.use("/api/admin", admin);
 app.use("/api/students", student);
+app.use("/api/teachers", teacher);
 app.use("/api/attendance", attandance);
